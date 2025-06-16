87eb185b605701e37f20d254946699c13bae2e69
nisel <nisel11good@gmail.com>
2025-06-16 19:42:16 +0300

    Dev

--- includes.container/usr/share/init.d/010-fsguard.sh
+++ includes.container/usr/share/init.d/010-fsguard.sh
@@ -37,7 +37,7 @@ function failed() {
     fi
 }
 
-/usr/libexec/FsGuard verify "/.system/FsGuard/filelist"
+/usr/lib/planifolia/FsGuard verify "/.system/FsGuard/filelist"
 if [[ $? -ne 0 ]]; then
     failed
 fi
